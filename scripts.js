0d41dde0fb022a965cfe34cb8248e1b891bbc4a5
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-08 13:29:53 +0530

    Logical operators

--- scripts.js
+++ scripts.js
@@ -1,49 +1,21 @@
-
+// Logical operators
 
 var a = 20;
-var b = "20";
-var c = 5
-var d = 15;
+var b = 10;
+var c = 5;
 
 
-//equal to
-if(a === b){
-    //equal value equal type
-    if(a === b ){
-        console.log("both value and type is equal");
-    }
-    //not equal value or not equal type
-    else if(a !== b){
-        console.log("only value or type is a equal to b");
-    }
+if (a == 10 && a == b){
+    console.log("its goood")
+
 }
 
-//Greater than or eqal
-// else if(a => 10){
-//     //geter than
-//     if(a > 10){
-//         console.log('a is gater than 10');
-//     }
-// }
+//or
+else if(a < 10 || c > b){
+    console.log("one is Rigth");
+}
 
-
-// else if (a != c){
-//    if(a < c) {
-//     console.log('a is less than c');
-//    }
-//    else{
-//     console.log('a is grater than c');
-//    }
-// }
-
-else{
-    // if(c <= 5){
-    //     console.log('C is eqal or  less than 5');
-    // }
-    // else{
-    //     console.log('C is eqal or  less than 5');
-    // }
-
-    //ternary oparater
-    c <= 5 ? console.log('C is eqal or  less than 5') :  console.log('C is eqal or  less than 5');
+//Not
+else if (a != c){
+    console.log('its normal');
 }
