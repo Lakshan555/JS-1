cf294b8b7b642abaff3b66a392e634c15688f592
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-08 13:59:30 +0530

    Switch case

--- scripts.js
+++ scripts.js
@@ -1,21 +1,38 @@
-// Logical operators
+// Switch 
 
-var a = 20;
-var b = 10;
-var c = 5;
+var empName = 'Ishanka';
+var jobRole = 'Doctor';
 
 
-if (a == 10 && a == b){
-    console.log("its goood")
-
+switch(jobRole.toLocaleLowerCase()){
+    case 'teacher': console.log(empName + 'is a teacher');
+    break;
+    case 'doctor':
+    case 'profeser': console.log(empName + 'is a doctor');
+    break;
+    case 'enginer' :  console.log(empName + 'is a teacher');
+    break;
+    case 'singer' : console.log(empName + 'is a singer');
+    break;
+    default: console.log(empName + ' is a somthing else');
+    break;
 }
 
-//or
-else if(a < 10 || c > b){
-    console.log("one is Rigth");
-}
+//-------------------------------
+var age = 20;
 
-//Not
-else if (a != c){
-    console.log('its normal');
-}
+switch(ture){
+    case age < 20 :
+        console.log(empName + 'is under Age');
+        break;
+    case age >= 20 && age < 30 : 
+        console.log(empName + 'is in pefect Age');
+        break;
+    case age >= 30 && age < 50:
+        console.log(empName + 'is in Mature Age');
+        break;
+    default :
+        console.log(empName + 'is Too old');
+    
+
+} 
