0f9042d9efc89316aa4a1bc3afbbbe3f2bb5f263
D.I.P Lakshan <onlineipl18@gmail.com>
2022-02-08 10:50:48 +0530

    Varibales

--- scripts.js
+++ scripts.js
@@ -1,2 +1,27 @@
-//print 
-console.log("Hello extrenal");
+//create variables
+var fistName = 'ishanka'; 
+let lastName = 'Pramidu'; 
+var age = 26;
+var isOk = true;
+var job,height;
+
+//print variable
+console.log('My Name is' + fistName + " " + lastName);
+
+//Get variable type
+console.log(typeof(fistName));
+console.log(typeof(age));
+console.log(typeof(isOk));
+
+//Difine
+job = 'Doctor';
+lastName = 'laskhan';
+
+console.log('i am ' + lastName + ' i am a ' + job);
+
+//get user input 
+height = prompt("What is your higth?");
+alert('My hight is ' + height + ' feet');
+
+
+
